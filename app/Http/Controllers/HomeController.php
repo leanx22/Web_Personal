5342498888094b727e1b465e354bf63b067d62d0
Lean <leandro.guia.dev@gmail.com>
2024-02-09 18:37:35 -0300

    Quite el incremento de estadísticas desde el HomeController, ya que fue reemplazado por la API. Me había olvidado.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,17 +10,6 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
-        ///////CREAR RUTA API PARA OBTENER LOS PROJS, INCLUIR LOGICA EN EL FRONT////////
-        $projects = Project::where('visible', 1)->orderBy('order','asc')->get();
-        
-        foreach($projects as $project)
-        {
-            $project->tagCollection = explode(",",$project->tags);
-        }
-
-        $odd = count($projects) % 2 == 0 ? true:false;
-        //////////////////////////////////////////////////////////////////////////////
-
-        return view('index',['projects'=>$projects, 'odd'=>$odd]);
+        return view('index');
     }
 }
