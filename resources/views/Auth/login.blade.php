7b5eeab0545acb71974c264ea8ed337276107b4c
Lean <leandro.guia.dev@gmail.com>
2024-02-29 22:05:13 -0300

    Nueva vista para inicio de sesion

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -8,16 +8,16 @@
 <script type="module" src="{{asset('js/auth/login.js')}}"></script>
 @endsection
 
-<body class="bg-stone-800">
+<body class="bg-stone-900">
   <div class="flex h-screen w-screen p-4 items-center justify-center">
-    <div class="flex flex-col items-center justify-center bg-white p-2 rounded-md shadow-lg">
-      <h1 class="font-semibold text-[24px] text-gray-500">Inicio de sesión</h1>
+    <div class="flex flex-col items-center justify-center bg-stone-800 p-2 rounded-md shadow-lg text-gray-200">
+      <h1 class="font-semibold text-[24px]">Inicio de sesión</h1>
       <form id="loginForm" class="mt-1 p-1 rounded-lg" method="POST">
         @csrf
-        <input name="email" type="email" class="bg-gray-200 p-2 rounded-lg" placeholder="Correo" required>
-        <input name="password" type="password" class="bg-gray-200 p-2 rounded-lg" placeholder="Clave" required><br>
-        <button class="bg-blue-500 rounded-full p-2 w-full mt-2 font-semibold text-white
-        shadow-md hover:bg-blue-600 auto_fade" id="sendBtn" type="button">Iniciar sesión</button>
+        <input name="email" type="email" class="bg-stone-900 p-2 rounded-lg outline-none ring-none ring-stone-600 focus:ring-1" placeholder="Correo" required>
+        <input name="password" type="password" class="bg-stone-900 p-2 rounded-lg outline-none ring-none ring-stone-600 focus:ring-1" placeholder="Clave" required><br>
+        <button class="bg-stone-900 border border-stone-700 rounded-lg p-2 w-full mt-2 font-semibold text-white
+        shadow-md hover:bg-indigo-600 hover:border-indigo-600 auto_fade" id="sendBtn" type="button">Iniciar sesión</button>
         <div id="loader_container" class="bg-gray-500 mt-2 rounded-lg p-2 flex items-center hidden">
             <img src="{{asset('img/gifs/loaders/loadCircle.gif')}}" class="max-h-[32px]">
             <label class="text-white ms-2">Iniciando sesion...</label>
